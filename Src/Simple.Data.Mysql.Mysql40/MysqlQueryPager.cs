42e4df8623278ba43c90e7ab8bbe8903b738dbd7
vidarls <vidar.l.somme@gmail.com>
2012-04-11 11:04:51 +0200

    Updated to work with Simple.Data 0.16.0.0

--- Src/Simple.Data.Mysql.Mysql40/MysqlQueryPager.cs
+++ Src/Simple.Data.Mysql.Mysql40/MysqlQueryPager.cs
@@ -10,6 +10,13 @@ namespace Simple.Data.Mysql.Mysql40
     [Export(typeof(IQueryPager))]
     public class MysqlQueryPager : IQueryPager
     {
+        public IEnumerable<string> ApplyLimit(string sql, int take)
+        {
+            var sb = new StringBuilder(sql);
+            sb.AppendFormat(" LIMIT {0}", take);
+            yield return sb.ToString();
+        }
+
         public IEnumerable<string> ApplyPaging(string sql, int skip, int take)
         {
             var sb = new StringBuilder(sql);
