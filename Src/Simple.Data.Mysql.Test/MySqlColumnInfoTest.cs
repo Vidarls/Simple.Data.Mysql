3b5136921bdf56f3172b3e51b321708857499fe8
Juanje Garcia <jjgarcia@ofi.es>
2013-09-20 18:07:36 +0200

    mysql decimal columns parsed to c# decimal type

---
+++ Src/Simple.Data.Mysql.Test/MySqlColumnInfoTest.cs
@@ -0,0 +1,30 @@
+using System.Data;
+using NUnit.Framework;
+
+namespace Simple.Data.Mysql.Test
+{
+    [TestFixture]
+    public class MySqlColumnInfoTest
+    {
+        [Test]
+        public void DecimalColumnWithCapacityAndPrecisionParsedToDecimal()
+        {
+            var columnInfo = MysqlColumnInfo.CreateColumnInfo("Price", "Price", "decimal(10,2)", "price");
+            Assert.True(columnInfo.DbType == DbType.Decimal);
+            Assert.True(columnInfo.Capacity == 10);
+        }
+
+        public void DecimalColumnWithoutCapacityParsedToDecimal()
+        {
+            var columnInfo = MysqlColumnInfo.CreateColumnInfo("Price", "Price", "decimal", "price");
+            Assert.True(columnInfo.DbType == DbType.Decimal);
+        }
+        public void DecimalColumnWithCapacityWithoutPrecisionParsedToDecimal()
+        {
+            var columnInfo = MysqlColumnInfo.CreateColumnInfo("Price", "Price", "decimal(10)", "price");
+            Assert.True(columnInfo.DbType == DbType.Decimal);
+            Assert.True(columnInfo.Capacity == 10);
+        }
+        
+    }
+}
